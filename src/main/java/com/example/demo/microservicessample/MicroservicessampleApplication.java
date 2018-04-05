e092a28e46e4bae50194cf7853204a7593d578b7
vanithasomanna <38080294+vanithasomanna@users.noreply.github.com>
2018-04-05 19:16:34 +0530

    Create MicroservicessampleApplication.java

---
+++ src/main/java/com/example/demo/microservicessample/MicroservicessampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.microservicessample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicessampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicessampleApplication.class, args);
+	}
+}
