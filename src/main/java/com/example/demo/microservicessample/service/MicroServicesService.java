d2739fa7b6e996960fec2b31dba58db2dd79c82a
vanithasomanna <38080294+vanithasomanna@users.noreply.github.com>
2018-04-06 17:18:42 +0530

    Comments added for methods

--- src/main/java/com/example/demo/microservicessample/service/MicroServicesService.java
+++ src/main/java/com/example/demo/microservicessample/service/MicroServicesService.java
@@ -4,21 +4,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MicroServicesService {
-	
+
 	/**
-	 * To find the nth Fibonacci number
+	 * Fibonacci series starts with 0,1 To find the nth Fibonacci number
 	 * 
 	 * @param nthFibonacciNum
 	 * @return nth fibonacci number
 	 */
 	public int getNthFibonacciNumber(long nthFibonacciNum) {
-		// 0,1,2,3,5
+		// Input : 0,1,2,3,5
+		// variables to hold starting series of Fibonacci number
 		int num1 = 1;
 		int num2 = 0;
-		if(nthFibonacciNum<0) {
-			throw new IllegalArgumentException ("Wrong input: " + nthFibonacciNum);
-		}
-		
+		// loop to find the nth fibonacci number
 		for (int i = 0; i <= nthFibonacciNum; i++) {
 			int fibonacci = num1 + num2;
 			num1 = num2;
@@ -35,9 +33,12 @@ public class MicroServicesService {
 	 * @return reversed string
 	 */
 	public String reverseWords(String sentence) {
-		// how are you - woh era uoy
+		// Input and output - how are you - woh era uoy
+		// Word array to split the input sentence
 		String[] wordArray = sentence.split(" ");
+		// String builder to hold the reversed string builder
 		StringBuilder reversedStrBuilder = new StringBuilder();
+		// Loop the sentence entered word by word
 		for (String word : wordArray) {
 			StringBuilder reversedWord = new StringBuilder();
 			for (int i = word.length() - 1; i >= 0; i--) {
@@ -50,6 +51,7 @@ public class MicroServicesService {
 
 	/**
 	 * To return Three types of triangle - Equilateral, Isosceles or Scalene
+	 * 
 	 * @param sideDimension1
 	 * @param sideDimension2
 	 * @param sideDimension3
@@ -57,14 +59,18 @@ public class MicroServicesService {
 	 */
 	public String fetchTriangleType(int sideDimension1, int sideDimension2, int sideDimension3) {
 
+		/*
+		 * check for Equilateral triangle whose all sides are equal, else check for
+		 * Isosceles triangle which has two equal sides else it will return Scalene
+		 * where all three sides are not equal
+		 */
 		if (sideDimension1 == sideDimension2 && sideDimension2 == sideDimension3) {
 			return "Equilateral";
-		}
-		if (sideDimension1 == sideDimension2 || sideDimension2 == sideDimension3 || sideDimension1 == sideDimension3) {
+		} else if (sideDimension1 == sideDimension2 || sideDimension2 == sideDimension3
+				|| sideDimension1 == sideDimension3) {
 			return "Isosceles";
 		}
 		return "Scalene";
 	}
-	
 
 }
