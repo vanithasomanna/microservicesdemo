bdaab8bdf72246928ac47095ebb0fea2bc1d7ac2
vanithasomanna <38080294+vanithasomanna@users.noreply.github.com>
2018-04-06 17:14:55 +0530

    Update controller to return NumberFormatException

--- src/main/java/com/example/demo/microservicessample/controller/FibonacciNumberController.java
+++ src/main/java/com/example/demo/microservicessample/controller/FibonacciNumberController.java
@@ -1,5 +1,8 @@
 package com.example.demo.microservicessample.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
@@ -25,10 +28,17 @@ public class FibonacciNumberController {
 	 * @return Response Entity
 	 */
 	@RequestMapping(value = "/Fibonacci", method = RequestMethod.GET)
-	public ResponseEntity<Integer> getNthFibonacciNumber(@RequestParam("n")  long nthFibonacciNum) {
-		return ResponseEntity.status(HttpStatus.OK)
-                .cacheControl(CacheControl.noCache()).header("Pragma", "no-cache")
-                .body(microServicesService.getNthFibonacciNumber(nthFibonacciNum));
-
+	public ResponseEntity<Object> getNthFibonacciNumber(@RequestParam("n")  Object nthFibonacciNum) {
+		try {
+			Long nthFibNum = new Long(nthFibonacciNum.toString());
+			 return ResponseEntity.status(HttpStatus.OK)
+		                .cacheControl(CacheControl.noCache()).header("Pragma", "no-cache")
+		                .body(microServicesService.getNthFibonacciNumber(nthFibNum));
+		}catch(NumberFormatException numberFormatExcpetion){
+			Map<String,String> response = new HashMap<String, String>();
+			response.put("error", "Invalid parameter value");
+			return ResponseEntity.badRequest().body(response);
+		}
 	}
 }
+
