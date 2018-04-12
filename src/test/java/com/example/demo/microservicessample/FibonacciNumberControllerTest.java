7b355bcb23482cc4a19eb580759c21c124e78a64
vanithasomanna <38080294+vanithasomanna@users.noreply.github.com>
2018-04-12 17:52:49 +0530

    Test case updated

--- src/test/java/com/example/demo/microservicessample/FibonacciNumberControllerTest.java
+++ src/test/java/com/example/demo/microservicessample/FibonacciNumberControllerTest.java
@@ -2,7 +2,6 @@ package com.example.demo.microservicessample;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,13 +10,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.CacheControl;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -33,7 +29,7 @@ public class FibonacciNumberControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
-	@Mock
+	@Autowired
 	private FibonacciNumberController fibonacciNumberController;
 	@InjectMocks
 	private MicroServicesService microServicesService;
@@ -47,8 +43,6 @@ public class FibonacciNumberControllerTest {
 
 	@Test
 	public void testGetNthFibonacciNumberPositive() throws Exception {
-		given(this.fibonacciNumberController.getNthFibonacciNumber("10")).willReturn(ResponseEntity
-				.status(HttpStatus.OK).cacheControl(CacheControl.noCache()).header("Pragma", "no-cache").body("55"));
 		mockMvc.perform(get("/api/Fibonacci").param("n", n)).andExpect(status().isOk());
 	}
 
@@ -62,14 +56,28 @@ public class FibonacciNumberControllerTest {
 		assertNotEquals(122, microServicesService.getNthFibonacciNumber(10));
 	}
 
-	@Test
-	public void testGetNthFibonacciNumberException() throws Exception {
-		when(fibonacciNumberController.getNthFibonacciNumber("10")).thenThrow(new NumberFormatException());
-	}
 
 	@Test
 	public void testFibonacciSuccess() throws Exception {
-		mockMvc.perform(get("/api/Fibonacci").param("n", n)).andExpect(status().isOk());
+		mockMvc.perform(get("/api/Fibonacci?n="+ n))
+                .andExpect(status().isOk());
 	}
+	
+	@Test
+    public void testFibController() throws Exception {
+        this.mockMvc.perform(get("/api/Fibonacci").param("n", n).accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk());
 
+    }
+
+	@Test
+    public void testFibonacciApiInvalid() throws Exception {
+       try{
+    	   fibonacciNumberController.getNthFibonacciNumber(new String("abc"));
+       }catch(NumberFormatException e){
+    	   
+       }
+		
+	}
+	
 }
