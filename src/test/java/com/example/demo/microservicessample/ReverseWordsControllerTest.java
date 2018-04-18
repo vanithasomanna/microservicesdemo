dffd5e1174e9518e49b43f1a81db9198457909f0
vanithasomanna <38080294+vanithasomanna@users.noreply.github.com>
2018-04-18 12:07:35 +0000

    negative test cases included

--- src/test/java/com/example/demo/microservicessample/ReverseWordsControllerTest.java
+++ src/test/java/com/example/demo/microservicessample/ReverseWordsControllerTest.java
@@ -1,21 +1,17 @@
 package com.example.demo.microservicessample;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertNotEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.CacheControl;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -29,6 +25,7 @@ import com.example.demo.microservicessample.service.MicroServicesService;
 public class ReverseWordsControllerTest {
 
 	private String sentence = "how are you";
+	
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -45,18 +42,17 @@ public class ReverseWordsControllerTest {
 	}
 
 	@Test
-	public void testSuccessfulWordsReversal() throws Exception {
-		mockMvc.perform(get("/api/ReverseWords").param("sentence", sentence)).andExpect(status().isOk());
-	}
-
-	@Test
 	public void testReverseWordsControllerPositive() throws Exception {
-		mockMvc.perform(get("/api/ReverseWords").param("sentence", "how are you")).andExpect(status().isOk());
+		mockMvc.perform(get("/api/ReverseWords").param("sentence", sentence)).andExpect(status().isOk());
 	}
 
 	@Test
 	public void testReverseWordsServicesPositive() {
 		assertEquals("lla ", microServicesService.reverseWords("all"));
 	}
-
+	
+	@Test
+	public void testReverseWordsServicesNegative() throws Exception {
+		assertNotEquals("negative", microServicesService.reverseWords(sentence));
+	}
 }
